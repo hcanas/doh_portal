91235b9c762489ed62b86b2b2b8c17b0038416cb
hcanas <harveyanas@gmail.com>
2022-11-25 09:19:14 +0800

    fix date format

--- src/components/forms/InputDate.vue
+++ src/components/forms/InputDate.vue
@@ -1,21 +1,12 @@
 <script setup>
-  import { ref, watch, onMounted } from 'vue';
   import { DatePicker } from 'v-calendar';
   
   const props = defineProps(['value']);
   const emit = defineEmits(['update:value']);
-  
-  const date = ref(null);
-  
-  onMounted(() => {
-    date.value = props.value;
-    
-    watch(() => date.value, new_val => emit('update:value', new_val.toLocaleDateString()));
-  });
 </script>
 
 <template>
-  <date-picker class="w-full h-full" v-model="date">
+  <date-picker class="w-full h-full" :modelValue="value" @update:modelValue="val => $emit('update:value', val?.toLocaleString())" mode="date" :model-config="{ type: 'string', mask: 'YYYY-MM-DD' }" :masks="{ L: 'YYYY-MM-DD' }">
     <template v-slot="{ inputValue, togglePopover }">
       <input type="text" @click="togglePopover" :value="inputValue" class="w-full text-gray-600 px-2 py-1 border rounded-lg" readonly />
     </template>
